e7dcb7730712b4eb4275a35b2dad767fdb5c472a
pranit9029 <50900508+pranit9029@users.noreply.github.com>
2019-10-04 10:14:40 +0530

    Add files via upload

--- src/CustomerApp/Customer/CustomerApp.model.ts
+++ src/CustomerApp/Customer/CustomerApp.model.ts
@@ -1,5 +1,21 @@
+import{NgForm,FormGroup,FormControl,Validator,FormBuilder, Validators} from '@angular/forms'
+
+
 export class Customer{
     CustomerCode:string="";
     CustomerName:string="";
     CustomerAmount:number=0;
+    formCustomerGroup: FormGroup = null;// Create object of FormGroup
+    constructor(){
+        var _builder=new FormBuilder();
+        this.formCustomerGroup = _builder.group({}); //use the builder to create
+
+        this.formCustomerGroup.addControl("CustomerNameControl",
+                                            new FormControl('',Validators.required)
+        );
+    }
 }
+
+//Create : We Create the validation object model
+//Connect : we connect the UI
+//Check : isvalid,isDirty
